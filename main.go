584669f96586a9e4fa8bea8a4f9fab0d41bfe850
Kyle Leaders <remkade@gmail.com>
2016-11-20 11:00:08 -0800

    Now pulls instance info from ASG

--- main.go
+++ main.go
@@ -1,31 +1,120 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	log "github.com/Sirupsen/logrus"
 	"github.com/aws/aws-sdk-go/aws"
 	"github.com/aws/aws-sdk-go/aws/session"
-	"github.com/aws/aws-sdk-go/services/ec2"
+	"github.com/aws/aws-sdk-go/service/autoscaling"
+	"github.com/aws/aws-sdk-go/service/ec2"
+	//"github.com/aws/aws-sdk-go/service/sns"
 )
 
+type config struct {
+	ASGName   string
+	Region    string
+	LogLevel  string
+	LogFormat string
+}
+
+var conf config
+
 func init() {
-	log.SetFormatter(&log.JSONFormatter{})
+	flag.StringVar(&conf.ASGName, "asg-name", "", "The autoscaling group's name")
+	flag.StringVar(&conf.Region, "region", "us-west-2", "The region name")
+	flag.StringVar(&conf.LogLevel, "log-level", "info", "Levels: panic|fatal|error|warn|info|debug")
+	flag.StringVar(&conf.LogFormat, "log-format", "plain", "Supports json or plain")
+	flag.Parse()
+
+	switch conf.LogLevel {
+	case "panic":
+		log.SetLevel(log.PanicLevel)
+	case "fatal":
+		log.SetLevel(log.FatalLevel)
+	case "error":
+		log.SetLevel(log.ErrorLevel)
+	case "warn":
+		log.SetLevel(log.WarnLevel)
+	case "info":
+		log.SetLevel(log.InfoLevel)
+	case "debug":
+		log.SetLevel(log.DebugLevel)
+	}
+
+	if conf.LogFormat == "json" {
+		log.SetFormatter(&log.JSONFormatter{})
+	}
 }
 
 func main() {
+
 	sess, err := session.NewSession()
 	if err != nil {
 		log.WithFields(log.Fields{
 			"error": err,
-		}).Fatalf("Failed to create session")
+		}).Fatal("Failed to create session")
 	}
 
+	// Create all the services
 	as := autoscaling.New(sess)
+	ec2client := ec2.New(sess)
+
+	instanceIDs := GetASGInstanceIDs(conf.ASGName, as)
+	params := &ec2.DescribeInstancesInput{
+		InstanceIds: instanceIDs,
+	}
+
+	instances, err := ec2client.DescribeInstances(params)
+	if err != nil {
+		log.WithFields(log.Fields{
+			"error": err,
+		}).Fatal("Failed to describe instances")
+	}
+	fmt.Println(instances)
+}
+
+// GetASGInstanceIDs queries the AutoScalingGroup for the instances
+// returning only the Instance IDs
+func GetASGInstanceIDs(asgName string, as *autoscaling.AutoScaling) (ids []*string) {
 	params := &autoscaling.DescribeAutoScalingGroupsInput{
 		AutoScalingGroupNames: []*string{
-			aws.String(config.ASGName),
+			aws.String(conf.ASGName),
 		},
-		MaxRecords: aws.Int64(1),
-		NextToken:  aws.String("XmlString"),
+		MaxRecords: aws.Int64(100),
 	}
+	resp, err := as.DescribeAutoScalingGroups(params)
+
+	if err != nil {
+		fmt.Println(err.Error())
+		log.WithFields(log.Fields{
+			"asg-name": conf.ASGName,
+			"error":    err,
+		}).Fatal("Failed to describe auto scaling group")
+	}
+
+	log.WithFields(log.Fields{
+		"asg-name": conf.ASGName,
+		"value":    fmt.Sprintf("%+v", resp),
+	}).Debug("Described ASG")
+
+	if len(resp.AutoScalingGroups) == 0 {
+		log.WithFields(log.Fields{
+			"asg-name": conf.ASGName,
+			"error":    "Unknown autoscaling group",
+		}).Fatal("Failed to describe auto scaling group")
+	}
+
+	instances := resp.AutoScalingGroups[0].Instances
+
+	ids = make([]*string, len(instances))
+	for n, instance := range resp.AutoScalingGroups[0].Instances {
+		log.WithFields(log.Fields{
+			"asg-name":    conf.ASGName,
+			"instance-id": *instance.InstanceId,
+		}).Debug("Found instance id")
+		ids[n] = instance.InstanceId
+	}
+
+	return
 }
